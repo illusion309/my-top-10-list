f0a2eb98d5563bccda5cd2a52feaf384c3ad7bdd
IQBeats <iqmakesmusic@gmail.com>
2020-12-07 05:52:12 +0200

    added procfile

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
-from mdb import SearchMovie, MovieDetails
+from .mdb import SearchMovie, MovieDetails
 import os
 
 app = Flask(__name__) 
